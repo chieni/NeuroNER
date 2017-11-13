6c33bec92fa38772aa2d3a8a7a1fcc790f66867f
chieni <mitchieni@gmail.com>
2017-11-13 15:11:05 -0500

    new processing

--- src/pycci_to_brat.py
+++ src/pycci_to_brat.py
@@ -2,6 +2,7 @@ import re
 import pandas as pd
 import os
 import spacy
+import difflib
 
 def get_start_and_end_offset_of_token_from_spacy(token):
 	start = token.idx
@@ -30,70 +31,9 @@ def get_sentences_and_tokens_from_spacy(text, spacy_nlp):
 		sentences.append(sentence_tokens)
 	return sentences
 
-def convert_to_brat(notes_filename, annotation_filename, labels, outpath):
-	spacy_nlp = spacy.load('en') 
-	# Convert data to BRAT format
-	label_df = pd.read_csv(annotation_filename, index_col=2, header=0)
-	notes_df = pd.read_csv(notes_filename, index_col=1, header=0)
-
-	# Possible labels
-	note_count = 1
-	breakpoint = (notes_df.shape[0]/2) + 2
-	for hadm_id, row in notes_df.iterrows():
-		# Get text
-		note = row['TEXT']
-		# Get annotated row
-		label_row = label_df.loc[hadm_id]
-		# TODO: handle multiple rows with same hadm_id
-
-		cleaned_note = " ".join(note.split())
-		# Get start and end indices for each kind of label
-		count = 1
-		write_list = []
-		for label, labelname in labels.items():
-			if label_row[label] == 1:
-				# Get label
-				start = int(label_row[label + ":start"])
-				end = int(label_row[label + ":end"])
-				phrase = cleaned_note[start:end]
-				assert phrase == " ".join(label_row[label + " Text"].split())
-				# Break into tokens (words), clean out punctuation, get start:end
-				to_write_list, count = create_annotation_file(count, phrase, labelname, start, spacy_nlp)
-				write_list += to_write_list
-
-		num = format(note_count, '05d')
-		note_count += 1
-		# Write to file
-		if note_count < breakpoint:
-			path = outpath + 'train'
-			if not os.path.exists(path):
-				os.mkdir(path)
-			note_file = open(path + '/train_text_' + str(num) + '.txt', 'w')
-			note_file.write(cleaned_note)
-			note_file.close()
-
-			anno_file = open(path + '/train_text_' + str(num) + '.ann', 'w')
-			anno_file.writelines(write_list)
-			anno_file.close()
-		else:
-			path = outpath + 'valid'
-			if not os.path.exists(path):
-				os.mkdir(path)
-			note_file = open(path + '/valid_text_' + str(num) + '.txt', 'w')
-			note_file.write(cleaned_note)
-			note_file.close()
-
-			anno_file = open(path + '/valid_text_' + str(num) + '.ann', 'w')
-			anno_file.writelines(write_list)
-			anno_file.close()
-
-def create_annotation_file(count, phrase, labelname, start_pos, spacy_nlp):
-	# Remove punctuation
-	sentences = get_sentences_and_tokens_from_spacy(phrase, spacy_nlp)
-	tokens = [item for sentence in sentences for item in sentence]
+def _create_annotation_output(count, tokens, labelname, start_pos):
 	to_write_list = []
 	new_count = int(count)
-	print(tokens)
 	for token in tokens:
 		content = token['text']
 		if not content.isalnum():
@@ -104,6 +44,101 @@ def create_annotation_file(count, phrase, labelname, start_pos, spacy_nlp):
 		new_count += 1
 	return to_write_list, new_count
 
-labels = {'Care Preferences': 'CAR', 'Family Meetings': 'FAM', 'Code Status Limitations': 'COD', 'Palliative Care Involvement': 'PAL'}
+def _convert_note_to_brat_format(note_count, note, cleaned_note, label_rows, labels, spacy_nlp):
+	# Get start and end indices for each kind of label
+	count = 1
+	write_list = []
+	for label_id, label_row in label_rows.iterrows():
+		label_note = label_row['TEXT'].replace('\r\r', '\n').replace('\r', '')
+		if note != label_note:
+			continue
+		for label, labelname in labels.items():
+			if label in label_row:
+				if label_row[label] == 1:
+					# Get label
+					start = int(label_row[label + ":start"])
+					end = int(label_row[label + ":end"])
+					phrase = cleaned_note[start:end]
+					assert phrase == " ".join(label_row[label + " Text"].split())
+					# Break into tokens (words), clean out punctuation, get start:end
+					sentences = get_sentences_and_tokens_from_spacy(phrase, spacy_nlp)
+					tokens = [item for sentence in sentences for item in sentence]
+					to_write_list, count = _create_annotation_output(count, tokens, labelname, start)
+					write_list += to_write_list
+	return write_list
+
+def convert_to_brat(filepath, labels, outpath):
+	spacy_nlp = spacy.load('en') 
+	if not os.path.exists(outpath):
+		os.mkdir(outpath)
+	annotated_file_list = os.listdir(filepath + 'Annotated Notes')
+	note_count = 1
+	set_of_ids = set()
+	for file in annotated_file_list:
+		notes_filename = filepath + 'Unannotated Notes/' + file[:-11] + '.csv'
+		label_df = pd.read_csv(filepath + 'Annotated Notes/' + file, index_col=2, header=0)
+		notes_df = pd.read_csv(notes_filename, index_col=1, header=0)
+
+		# Possible labels
+		for hadm_id, row in notes_df.iterrows():
+			# Get text
+			note = row['TEXT'].replace('\r','')
+			cleaned_note = " ".join(note.split())
+
+			# Get annotated rows
+			label_rows = label_df.loc[hadm_id]
+			if len(label_rows.shape) == 1:
+				label_rows = label_rows.to_frame().transpose()
+			
+			write_list = _convert_note_to_brat_format(note_count, note, cleaned_note, label_rows, labels, spacy_nlp)
+			num = format(note_count, '05d')
+			note_count += 1
+
+			#Write to file
+			note_file = open(outpath + 'text_' + str(num) + '.txt', 'w')
+			note_file.write(cleaned_note)
+			note_file.close()
+
+			anno_file = open(outpath + 'text_' + str(num) + '.ann', 'w')
+			anno_file.writelines(write_list)
+			anno_file.close()
+
+
+def split_data_sets(filepath):
+	input_filepath = filepath + 'output/'
+	file_list = os.listdir(input_filepath)
+	training_ratio = 0.7
+	breakpoint = (len(file_list)/2)*0.7
+
+	if not os.path.exists(filepath + 'train'):
+		os.mkdir(outpath + 'train')
+	if not os.path.exists(filepath + 'valid'):
+		os.mkdir(outpath + 'valid')
+
+	for i in range(0,len(file_list),2):
+		filename = file_list[i].split('.')[0]
+		if i/2 < breakpoint:
+			outfilepath = filepath + 'train/train_'
+		else:
+			outfilepath = filepath + 'valid/valid_'
+
+		with open(input_filepath + filename + '.txt') as f:
+			lines = f.readlines()
+			with open(outfilepath + filename + '.txt', 'w') as g:
+				g.writelines(lines)
+		with open(input_filepath + filename + '.ann') as a:
+			ann_lines = a.readlines()
+			with open(outfilepath + filename + '.ann', 'w') as b:
+				b.writelines(ann_lines)
+
+
+labels = {"Patient and Family Care Preferences": 'CAR',
+"Communication with Family":'FAM',
+"Full Code Status": 'COD',
+"Code Status Limitations": 'LIM',
+"Palliative Care Team Involvement": 'PAL'}
+
 outpath = '../data/goals_of_care/'
-convert_to_brat('../data/goals_of_care/notes.csv','../data/goals_of_care/labelled.csv', labels, outpath)
+directory = '/Users/IsabelChien/Dropbox (MIT)/Goals_of_Care_Notes/'
+#convert_to_brat(directory, labels, outpath)
+#split_data_sets(outpath)
