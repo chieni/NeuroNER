01b4afbc4cbdcf61ffd373ed42f57262a732408e
chieni <mitchieni@gmail.com>
2018-02-05 18:56:17 -0500

    .

--- src/bootstrap_FAM.sh
+++ src/bootstrap_FAM.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for i in {1..1000}; do
 	echo $i
-	echo "CUDA_VISIBLE_DEVICES=3 python3.5 main.py --train_model=False --use_pretrained_model=True --dataset_text_folder=../data/FAM_bootstrapping/trial_$i/ --pretrained_model_folder=../trained_models/fam_model --output_folder=../output/FAM_bootstrapping/trial_$i/ >> bootstrap_FAM.txt"
-	CUDA_VISIBLE_DEVICES=3 python3.5 main.py --train_model=False --use_pretrained_model=True --dataset_text_folder=../data/FAM_bootstrapping/trial_$i/ --pretrained_model_folder=../trained_models/fam_model --output_folder=../output/FAM_bootstrapping/trial_$i/ >> bootstrap_FAM.txt
+	echo "CUDA_VISIBLE_DEVICES=0 python3.5 main.py --train_model=False --use_pretrained_model=True --dataset_text_folder=../data/FAM_bootstrapping/trial_$i/ --pretrained_model_folder=../trained_models/fam_model --output_folder=../output/FAM_bootstrapping/trial_$i/ >> bootstrap_FAM.txt"
+	CUDA_VISIBLE_DEVICES=0 python3.5 main.py --train_model=False --use_pretrained_model=True --dataset_text_folder=../data/FAM_bootstrapping/trial_$i/ --pretrained_model_folder=../trained_models/fam_model --output_folder=../output/FAM_bootstrapping/trial_$i/ >> bootstrap_FAM.txt
 done
--- src/bootstrap_LIM.sh
+++ src/bootstrap_LIM.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for i in {39..1000}; do
 	echo $i
-	echo "CUDA_VISIBLE_DEVICES=3 python3.5 main.py --train_model=False --use_pretrained_model=True --dataset_text_folder=../data/LIM_bootstrapping/trial_$i/ --pretrained_model_folder=../trained_models/lim_model --output_folder=../output/LIM_bootstrapping/trial_$i/ >> bootstrap_LIM.txt"
-	CUDA_VISIBLE_DEVICES=3 python3.5 main.py --train_model=False --use_pretrained_model=True --dataset_text_folder=../data/LIM_bootstrapping/trial_$i/ --pretrained_model_folder=../trained_models/lim_model --output_folder=../output/LIM_bootstrapping/trial_$i/ >> bootstrap_LIM.txt
+	echo "CUDA_VISIBLE_DEVICES=1 python3.5 main.py --train_model=False --use_pretrained_model=True --dataset_text_folder=../data/LIM_bootstrapping/trial_$i/ --pretrained_model_folder=../trained_models/lim_model --output_folder=../output/LIM_bootstrapping/trial_$i/ >> bootstrap_LIM.txt"
+	CUDA_VISIBLE_DEVICES=1 python3.5 main.py --train_model=False --use_pretrained_model=True --dataset_text_folder=../data/LIM_bootstrapping/trial_$i/ --pretrained_model_folder=../trained_models/lim_model --output_folder=../output/LIM_bootstrapping/trial_$i/ >> bootstrap_LIM.txt
 done
