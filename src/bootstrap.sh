00c1e555fdfcc1ff1206996e2cc088ce5fa9337f
chieni <mitchieni@gmail.com>
2018-02-05 01:11:44 -0500

    .

--- src/bootstrap.sh
+++ src/bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for i in {1..1000}; do
-	echo "this.py $i &"
-	echo "this.py $i &" >> bootstrap.txt
+	echo $i
+	CUDA_VISIBLE_DEVICES=3 python3.5 main.py --train_model=False --use_pretrained_model=True --dataset_text_folder=../data/CAR_bootstrapping/trial_$i/ --pretrained_model_folder=../trained_models/car_model output_folder_name >> bootstrap_CAR.txt
 done
