51d3b98d7ebd2fb7a7437681eb93fea05ab0e8fa
chieni <mitchieni@gmail.com>
2018-02-08 00:02:57 -0500

    .

--- src/bootstrap_FAM.sh
+++ src/bootstrap_FAM.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for i in {1000..1209}; do
+for i in {1000..1210}; do
 	echo $i
 	echo "CUDA_VISIBLE_DEVICES=0 python3.5 main.py --train_model=False --use_pretrained_model=True --dataset_text_folder=../data/FAM_bootstrapping/trial_$i/ --pretrained_model_folder=../trained_models/fam_model --output_folder=../output/FAM_bootstrapping/trial_$i/ >> bootstrap_FAM.txt"
 	CUDA_VISIBLE_DEVICES=0 python3.5 main.py --train_model=False --use_pretrained_model=True --dataset_text_folder=../data/FAM_bootstrapping/trial_$i/ --pretrained_model_folder=../trained_models/fam_model --output_folder=../output/FAM_bootstrapping/trial_$i/ >> bootstrap_FAM.txt
--- src/bootstrap_LIM.sh
+++ src/bootstrap_LIM.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for i in {1000..1184}; do
+for i in {1000..1185}; do
 	echo $i
 	echo "CUDA_VISIBLE_DEVICES=1 python3.5 main.py --train_model=False --use_pretrained_model=True --dataset_text_folder=../data/LIM_bootstrapping/trial_$i/ --pretrained_model_folder=../trained_models/lim_model --output_folder=../output/LIM_bootstrapping/trial_$i/ >> bootstrap_LIM.txt"
 	CUDA_VISIBLE_DEVICES=1 python3.5 main.py --train_model=False --use_pretrained_model=True --dataset_text_folder=../data/LIM_bootstrapping/trial_$i/ --pretrained_model_folder=../trained_models/lim_model --output_folder=../output/LIM_bootstrapping/trial_$i/ >> bootstrap_LIM.txt
