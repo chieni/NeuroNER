dfd38141c5536aaa86a57666bd38043ae742b799
chieni <mitchieni@gmail.com>
2018-02-09 00:58:26 -0500

    more cim

--- src/bootstrap_CIM.sh
+++ src/bootstrap_CIM.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # Training LIM model on CAR data
-for i in {0..1012}; do
+for i in {1..1001}; do
 	echo $i
-	echo "CUDA_VISIBLE_DEVICES=3 python3.5 main.py --train_model=False --use_pretrained_model=True --dataset_text_folder=../data/CAR_bootstrapping/trial_$i/ --pretrained_model_folder=../trained_models/lim_model --output_folder=../output/CIM_bootstrapping/trial_$i/ >> bootstrap_CIM.txt"
-	CUDA_VISIBLE_DEVICES=3 python3.5 main.py --train_model=False --use_pretrained_model=True --dataset_text_folder=../data/CAR_bootstrapping/trial_$i/ --pretrained_model_folder=../trained_models/lim_model --output_folder=../output/CIM_bootstrapping/trial_$i/ >> bootstrap_CIM.txt
+	echo 'CAR'
+	CUDA_VISIBLE_DEVICES=3 python3.5 main.py --train_model=False --use_pretrained_model=True --dataset_text_folder=../data/CIM_bootstrapping/CAR/trial_$i/ --pretrained_model_folder=../trained_models/car_model --output_folder=../output/CIM_bootstrapping/CAR/trial_$i/ >> bootstrap_CIM_CAR.txt
+	echo 'LIM'
+	CUDA_VISIBLE_DEVICES=2 python3.5 main.py --train_model=False --use_pretrained_model=True --dataset_text_folder=../data/CIM_bootstrapping/LIM/trial_$i/ --pretrained_model_folder=../trained_models/lim_model --output_folder=../output/CIM_bootstrapping/LIM/trial_$i/ >> bootstrap_CIM_LIM.txt
 done
