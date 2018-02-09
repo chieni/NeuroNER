be347d79b701e5959683b0d8239c7bbf39d21830
chieni <mitchieni@gmail.com>
2018-02-09 00:47:43 -0500

    cim bootstrapping

--- src/bootstrap_cim.py
+++ src/bootstrap_cim.py
@@ -23,34 +23,36 @@ def sample_test_data_cim(car_folder, lim_folder, start, end):
 	# Read files from original data folder
 	car_original_dir = '../data/' + car_folder + '/valid/'
 	lim_original_dir = '../data/' + lim_folder + '/valid/'
-	car_files = os.listdir(car_original_dir)
-	car_files = [file[:11] for file in car_files if file[-3:] == 'txt']
-	car_set = set(car_files)
+	files = os.listdir(car_original_dir)
+	files = [file[:11] for file in files if file[-3:] == 'txt']
 
-	lim_files = os.listdir(lim_original_dir)
-	lim_files = [file[:11] for file in lim_files if file[-3:] == 'txt']
-	lim_set = set(lim_files)
+	for i in range(start, end):
+		print(i)
+		# get random sample of files with replacementcd ..cd 
+		test_files = resample(files)
+		assert len(test_files) == len(files)
+		os.mkdir(car_output_dir + '/trial_' + str(i))
+		os.mkdir(lim_output_dir + '/trial_' + str(i))
+		car_test_dir = car_output_dir + '/trial_' + str(i) + '/test/'
+		lim_test_dir = lim_output_dir + '/trial_' + str(i) + '/test/'
+		os.mkdir(car_test_dir)
+		os.mkdir(lim_test_dir)
+		# Copy these files into test_dir
 
-	print(len(lim_set.intersection(car_set)))
+		for i, note_name in enumerate(test_files):
+			# For CAR data
+			note_content = open(car_original_dir + note_name + '.txt', 'r').readlines()
+			note_file = open(car_test_dir + note_name +  '_' + str(i) + '.txt', 'w').writelines([l for l in note_content])
 
+			ann_content = open(car_original_dir + note_name + '.ann', 'r').readlines()
+			ann_file = open(car_test_dir + note_name +  '_' + str(i) + '.ann', 'w').writelines([l for l in ann_content])
 
-	# for i in range(start, end):
-	# 	print(i)
-	# 	# get random sample of files with replacementcd ..cd 
-	# 	test_files = resample(files)
-	# 	assert len(test_files) == len(files)
-	# 	os.mkdir(output_dir + '/trial_' + str(i))
-	# 	test_dir = output_dir + '/trial_' + str(i) + '/test/'
-	# 	os.mkdir(test_dir)
-	# 	# Copy these files into test_dir
+			# For LIM data
+			lim_note_content = open(lim_original_dir + note_name + '.txt', 'r').readlines()
+			lim_note_file = open(lim_test_dir + note_name +  '_' + str(i) + '.txt', 'w').writelines([l for l in lim_note_content])
 
-	# 	for i, note_name in enumerate(test_files):
-	# 		note_content = open(original_dir + note_name + '.txt', 'r').readlines()
-	# 		note_file = open(test_dir + note_name +  '_' + str(i) + '.txt', 'w').writelines([l for l in note_content])
-
-	# 		ann_content = open(original_dir + note_name + '.ann', 'r').readlines()
-	# 		ann_file = open(test_dir + note_name +  '_' + str(i) + '.ann', 'w').writelines([l for l in ann_content])
-
+			lim_ann_content = open(lim_original_dir + note_name + '.ann', 'r').readlines()
+			lim_ann_file = open(lim_test_dir + note_name +  '_' + str(i) + '.ann', 'w').writelines([l for l in lim_ann_content])
 
 if __name__ == '__main__':
 	sample_test_data_cim(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
