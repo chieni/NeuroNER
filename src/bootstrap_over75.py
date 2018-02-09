e8e250feb4b5f09ef7d78b555877b52865f0f44c
chieni <mitchieni@gmail.com>
2018-02-09 16:44:38 -0500

    .

--- src/bootstrap_over75.py
+++ src/bootstrap_over75.py
@@ -10,33 +10,33 @@ import sys
 # - Save list of folders
 
 
-def sample_test_data(start, end):
+def sample_test_data(filenum):
 	output_dir = '../data/over75_bootstrapping'
 	if not os.path.exists(output_dir):
 		os.mkdir(output_dir)
-	start, end = int(start), int(end)
+
 	# Read files from original data folder
 	original_dir = '../data/over_75/deploy/'
+
 	files = os.listdir(original_dir)
 	files = [file[:11] for file in files if file[-3:] == 'txt']
-	print(len(files))
-	for i in range(start, end):
-		print(i)
-		# get random sample of files with replacementcd ..cd 
-		test_files = resample(files)
-		assert len(test_files) == len(files)
-		os.mkdir(output_dir + '/trial_' + str(i))
-		test_dir = output_dir + '/trial_' + str(i) + '/deploy/'
-		os.mkdir(test_dir)
-		# Copy these files into test_dir
 
-		for i, note_name in enumerate(test_files):
-			note_content = open(original_dir + note_name + '.txt', 'r').readlines()
-			note_file = open(test_dir + note_name +  '_' + str(i) + '.txt', 'w').writelines([l for l in note_content])
+	print(filenum)
+	# get random sample of files with replacementcd ..cd 
+	test_files = resample(files)
+	assert len(test_files) == len(files)
+	os.mkdir(output_dir + '/trial_' + filenum)
+	test_dir = output_dir + '/trial_' + filenum + '/deploy/'
+	os.mkdir(test_dir)
+	# Copy these files into test_dir
 
-			ann_content = open(original_dir + note_name + '.ann', 'r').readlines()
-			ann_file = open(test_dir + note_name +  '_' + str(i) + '.ann', 'w').writelines([l for l in ann_content])
+	for i, note_name in enumerate(test_files):
+		note_content = open(original_dir + note_name + '.txt', 'r').readlines()
+		note_file = open(test_dir + note_name +  '_' + filenum + '.txt', 'w').writelines([l for l in note_content])
+
+		ann_content = open(original_dir + note_name + '.ann', 'r').readlines()
+		ann_file = open(test_dir + note_name +  '_' + filenum + '.ann', 'w').writelines([l for l in ann_content])
 
 
 if __name__ == '__main__':
-	sample_test_data(sys.argv[1], sys.argv[2])
+	sample_test_data(sys.argv[1])
--- src/bootstrap_over75.sh
+++ src/bootstrap_over75.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-# Training LIM model on CAR data
-for i in {1..1001}; do
-	echo $i
-	echo 'LIM'
-	CUDA_VISIBLE_DEVICES=2 python3.5 main.py --train_model=False --use_pretrained_model=True --dataset_text_folder=../data/over_75_bootstrapping/trial_$i/ --pretrained_model_folder=../trained_models/lim_model --output_folder=../output/over_75_bootstrapping/LIM/trial_$i >> bootstrap_over75_LIM.txt
-done
+
+# $1 filenum
+# Sample deploy
+#python3.5 bootstrap_over75.py $1
+
+# Run model for LIM, CAR, FAM
+echo $1
+echo "CUDA_VISIBLE_DEVICES=2 python3.5 main.py --train_model=False --use_pretrained_model=True --dataset_text_folder=../data/over_75_bootstrapping/trial_$1/ --pretrained_model_folder=../trained_models/lim_model --output_folder=../output/over_75_bootstrapping/LIM/trial_$1 >> bootstrap_over75_LIM.txt"
+# CUDA_VISIBLE_DEVICES=2 python3.5 main.py --train_model=False --use_pretrained_model=True --dataset_text_folder=../data/over_75_bootstrapping/trial_$1/ --pretrained_model_folder=../trained_models/lim_model --output_folder=../output/over_75_bootstrapping/LIM/trial_$1 >> bootstrap_over75_LIM.txt
+# CUDA_VISIBLE_DEVICES=0 python3.5 main.py --train_model=False --use_pretrained_model=True --dataset_text_folder=../data/over_75_bootstrapping/trial_$1/ --pretrained_model_folder=../trained_models/car_model --output_folder=../output/over_75_bootstrapping/CAR/trial_$1 >> bootstrap_over75_CAR.txt
+# CUDA_VISIBLE_DEVICES=1 python3.5 main.py --train_model=False --use_pretrained_model=True --dataset_text_folder=../data/over_75_bootstrapping/trial_$1/ --pretrained_model_folder=../trained_models/fam_model --output_folder=../output/over_75_bootstrapping/FAM/trial_$1 >> bootstrap_over75_FAM.txt
+# # Remove the file folder
+# rm -r ../data/over_75_bootstrapping/trial_$
