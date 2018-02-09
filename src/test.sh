d19e2ea544f92ed6b96131fb157c5c887822c7d5
chieni <mitchieni@gmail.com>
2018-02-09 14:45:00 -0500

    delete script

--- src/test.sh
+++ src/test.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-echo "CUDA_VISIBLE_DEVICES=$4 python3.5 main.py --train_model=False --use_pretrained_model=True --dataset_text_folder=../data/$1/$4 --pretrained_model_folder=../trained_models/$2 --output_folder=../output/$1/$4/ >>  $3"
-echo $1
-echo $2
-echo $3
-echo $4
-echo $5
-echo $6
-#CUDA_VISIBLE_DEVICES=$4 python3.5 main.py --train_model=False --use_pretrained_model=True --dataset_text_folder=../data/$1/$5 --pretrained_model_folder=../trained_models/$2 --output_folder=../output/$1/$5/ >>  $3
+echo "CUDA_VISIBLE_DEVICES=$4 python3.5 main.py --train_model=False --use_pretrained_model=True --dataset_text_folder=../data/$1/$5 --pretrained_model_folder=../trained_models/$2 --output_folder=../output/$1/$5/ >>  $3"
+CUDA_VISIBLE_DEVICES=$4 python3.5 main.py --train_model=False --use_pretrained_model=True --dataset_text_folder=../data/$1/$5 --pretrained_model_folder=../trained_models/$2 --output_folder=../output/$1/$5/ >>  $3
