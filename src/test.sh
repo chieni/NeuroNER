d5713ef8f8355f56a3e3a9ca895e10b8a6c02e7e
chieni <mitchieni@gmail.com>
2018-02-09 13:49:45 -0500

    .

--- src/test.sh
+++ src/test.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-echo $0
-echo $1
-echo $2
+echo 'CUDA_VISIBLE_DEVICES=2 python3.5 main.py --train_model=False --use_pretrained_model=True --dataset_text_folder=../data/$1/$4 --pretrained_model_folder=../trained_models/$2 --output_folder=../output/$1/$4/ >>  $3'
