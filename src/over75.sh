c6daa221990babfe2c8f839faa5fbbaabffc2117
chieni <mitchieni@gmail.com>
2018-02-13 16:46:00 -0500

    .

--- src/over75.sh
+++ src/over75.sh
@@ -3,6 +3,7 @@
 for i in {1..10}; do
 	echo $i
 	start_time=$(date)
+	echo "$start_time"
 	# Write line to bootstrap
 	CUDA_VISIBLE_DEVICES=2 python3.5 main.py --train_model=False --use_pretrained_model=True --dataset_text_folder=../data/over75_bootstrapping/trial_$i/ --pretrained_model_folder=../trained_models/lim_model --output_folder=../output/time_test/LIM/trial_$i
 	echo -e "LIM\t$start_time\t$(date)" >> test_runtime.txt
